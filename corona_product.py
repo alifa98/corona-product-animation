04cd707e06c85d92f07a72968f524d78d0e5ed6e
Ali Faraji <billa.svpc@gmail.com>
2021-05-07 18:37:06 +0430

    animation completed

--- corona_product.py
+++ corona_product.py
@@ -20,7 +20,7 @@ class CoronaProduct(Scene):
         title.to_corner(UP + LEFT)
         self.play(
             Transform(question_title, title),
-            LaggedStart(*[FadeOutAndShift(obj, direction=DOWN) for obj in formulaTextTitle]),
+            LaggedStart(*[FadeOutAndShift(obj, direction=DOWN) for obj in formulaTextTitle])
         )
         self.wait()
 
@@ -74,6 +74,94 @@ class CoronaProduct(Scene):
         self.play(Write(arrow_to_graph_g_text))
         self.wait()
 
-        arrow_to_graph_g_text_transformed = Tex("So We Copy the Graph H, 3 times", color=BLUE).scale(0.7).next_to(arrow_to_graph_g, RIGHT + UP)
-        self.play(Transform(arrow_to_graph_g_text, arrow_to_graph_g_text_transformed))
+        # point to to of graph H
+        arrow_to_graph_h = CurvedArrow(graph_h.get_center() + graph_h.width * RIGHT + 2 * UP, graph_h.get_center() + graph_h.width/2*RIGHT + graph_h.height/2*UP, radius=-8)
+        arrow_to_graph_h_text = Tex("So We Copy the Graph H, 3 times", color=BLUE).scale(0.7).next_to(arrow_to_graph_h, RIGHT + UP)
+
+        self.play(Transform(arrow_to_graph_g_text, arrow_to_graph_h_text),
+                  Transform(arrow_to_graph_g, arrow_to_graph_h))
+        self.wait(2)
+
+        self.play(
+            FadeOut(arrow_to_graph_g_text),
+            Uncreate(arrow_to_graph_g)
+        )
+
+        self.wait()
+
+        odotSymbol = MathTex(r" \odot ").scale(1.5).next_to(graph_g, RIGHT)
+
+        self.add(odotSymbol)
+        self.play(
+            Create(odotSymbol),
+            graph_h.animate.shift(RIGHT),
+            graph_h_title.animate.shift(RIGHT)
+        )
+
+        equalSymbol = MathTex(r" = ").scale(1.5).next_to(graph_h, 2 * RIGHT)
+        self.add(equalSymbol)
+        self.play(
+            Create(equalSymbol)
+        )
+
+        corona_product_graph_v = {1, 2, 3, 11, 12, 21, 22, 31, 32}
+
+        corona_product_graph_e = [(1, 2), (2, 3), (1, 3), (11, 12), (21, 22), (31, 32)]
+        corona_product_graph = Graph(
+            corona_product_graph_v,
+            corona_product_graph_e,
+            layout={
+                1: [1, 0, 0],
+                2: [-1, 0, 0],
+                3: [0, 1, 0],
+                11: [2, 0, 0],
+                12: [1, -1, 0],
+                21: [-2, 0, 0],
+                22: [-1, -1, 0],
+                31: [1, 2, 0],
+                32: [-1, 2, 0]
+            }
+        )
+        corona_product_graph.move_to(2 * RIGHT)
+
+        self.add(corona_product_graph)
+        self.play(Create(corona_product_graph))
+        self.wait()
+
+        description = Text("then we connect\nall vertices of each copies\nto correspondig vertex in G").scale(0.5).next_to(corona_product_graph, UP)
+
+        self.add(description)
+        self.play(Write(description))
+
+        self.play(corona_product_graph.animate.add_edges(
+            *[
+                (12, 1), (11, 1)
+            ],
+            edge_config={
+                (12, 1): {"stroke_color": RED},
+                (11, 1): {"stroke_color": RED}
+            }
+        ))
+
+        self.wait()
+        self.play(corona_product_graph.animate.add_edges(
+            *[
+                (21, 2), (22, 2)
+            ],
+            edge_config={
+                (21, 2): {"stroke_color": RED},
+                (22, 2): {"stroke_color": RED}
+            }
+        ))
+
+        self.play(corona_product_graph.animate.add_edges(
+            *[
+                (31, 3), (32, 3)
+            ],
+            edge_config={
+                (31, 3): {"stroke_color": RED},
+                (32, 3): {"stroke_color": RED}
+            }
+        ))
+
         self.wait()
